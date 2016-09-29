5a65412e4956b79d0ed10d323cd0c7d2cdd9dd72
Scott Miller <scott.miller171@gmail.com>
2016-09-29 17:53:45 -0400

    working no ajax

--- damagecalc/views.py
+++ damagecalc/views.py
@@ -2,7 +2,66 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from randomdotorg import RandomDotOrg
+
+
+def aceableRoll(skillLevel):
+    r = RandomDotOrg('DamageCalc')
+    result = 0
+    roll = skillLevel
+    while roll == skillLevel:
+        roll = r.randrange(1, skillLevel, 1)
+        result = result + roll
+    return result
+
+
+def role(skillLevel):
+    r = RandomDotOrg('DamageCalc')
+    result = 0
+    roll = r.randrange(1, skillLevel, 1)
+    result = result + roll
+    return result
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    shootingSkill = int(request.GET.get('shootingSkill', 4))
+    shootingRange = int(request.GET.get('range', 0))
+    cover = int(request.GET.get('cover', 0))
+    specialized = int(request.GET.get('specialized', 2))
+    mods = int(request.GET.get('mods', 0))
+    context = {
+        'shootingSkill': shootingSkill,
+        'shootingRange': shootingRange,
+        'cover': cover,
+        'specialized': specialized,
+        'mods': mods,
+    }
+    return render(request, 'damagecalc/index.html', context)
+
+
+def submit(request):
+    shootingSkill = int(request.GET.get('shootingSkill', 4))
+    shootingRange = int(request.GET.get('range', 0))
+    cover = int(request.GET.get('cover', 0))
+    specialized = int(request.GET.get('specialized', 2))
+    mods = int(request.GET.get('mods', 0))
+    rollResult = aceableRoll(shootingSkill)
+    print rollResult
+    result = (rollResult -
+              shootingRange -
+              cover -
+              specialized +
+              mods)
+    print rollResult
+    print result
+    context = {
+        'shootingSkill': shootingSkill,
+        'shootingRange': shootingRange,
+        'cover': cover,
+        'specialized': specialized,
+        'mods': mods,
+        'rollResult': rollResult,
+        'result': result,
+        'rolled': True,
+    }
+    return render(request, 'damagecalc/index.html', context)
--- python_damage_calc/urls.py
+++ python_damage_calc/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^favicon\.ico$', favicon_view),
+    url(r'', include('damagecalc.urls')),
 ]
